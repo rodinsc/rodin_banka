fdfdaac9613c95032b88e4dbd69ebc4e32401b13
Rodin <76735468+rodinsc@users.noreply.github.com>
2021-01-20 15:06:02 +0300

    Add files via upload

---
+++ __resource.lua
@@ -0,0 +1,32 @@
+resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
+
+ui_page('client/html/UI.html')
+
+server_scripts {  
+    '@mysql-async/lib/MySQL.lua',
+	'server.lua'
+}
+
+client_scripts {
+	'@es_extended/locale.lua',
+	'client/client.lua'
+}
+
+files {
+	'client/html/UI.html',
+    'client/html/style.css',
+    'client/html/media/font/Bariol_Regular.otf',
+    'client/html/media/font/Vision-Black.otf',
+    'client/html/media/font/Vision-Bold.otf',
+    'client/html/media/font/Vision-Heavy.otf',
+    'client/html/media/img/bg.png',
+    'client/html/media/img/circle.png',
+    'client/html/media/img/curve.png',
+    'client/html/media/img/fingerprint.png',
+    'client/html/media/img/fingerprint.jpg',
+    'client/html/media/img/graph.png',
+    'client/html/media/img/logo-big.png',
+    'client/html/media/img/nextRP.png',
+    'client/html/media/img/eur.png',
+    'client/html/media/img/usd.png'
+} 
